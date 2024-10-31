b0492e406d0a0476d3b99577ab1a9bc3255ec101
alissyaa <alisssyahumairah@gmail.com>
2024-10-31 09:37:33 +0700

    merapikan

--- home.php
+++ home.php
@@ -47,7 +47,7 @@ if(isset($_POST['submit'])){
     <title>BookBerry</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"> <!-- Link Poppins -->
-    <link href="styles.css" rel="stylesheet">
+    <link href="style/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 </head>
 <body>
