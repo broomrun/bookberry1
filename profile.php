16047b91ab6c2ef847b9a4d3984e6661c59e6bed
alissyaa <alissyahumairah@gmail.com>
2024-11-05 13:32:18 +0700

    semoga ada

--- profile.php
+++ profile.php
@@ -32,7 +32,6 @@
         <div class="stats">
             <span><strong>550</strong> friends </span>
             <span><strong>40</strong> streak </span>
-            <span><strong>3</strong> badges </span>
             <span><strong>70</strong> books </span>
         </div>
     </div>
